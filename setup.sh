6469ff12ea6cf270ec303584a53d2c07848a363f
lisi-123 <160303845+lisi-123@users.noreply.github.com>
2025-05-15 00:48:28 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,20 +5,35 @@ apt-get update
 
 # 安装必需的软件包
 apt install sudo -y
-apt install wget -y
+sudo apt install git -y
 apt install -y jq
 sudo apt install curl -y
 sudo apt install nano -y
 
-# 设置iptables规则
+# 拉取库
+until git clone https://github.com/lisi-123/private.git; do
+  echo "git clone 失败，3 秒后重试..."; sleep 3;
+done
+
+# 赋予 socks5-check.sh 可执行权限
+chmod +x /root/v2bx-scr/socks5-check.sh
+
+# 检测并添加虚拟内存
+chmod +x /root/v2bx-scr/swap.sh && /root/v2bx-scr/swap.sh
+
+# 安装 iptables-persistent（自动回答“是”）
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y iptables-persistent
+
+# 添加规则（UDP 35000-36000 转发到 50000）
 sudo iptables -t nat -A PREROUTING -p udp --dport 35000:36000 -j REDIRECT --to-port 50000
 
 # 保存规则
 mkdir -p /etc/iptables
 sudo iptables-save > /etc/iptables/rules.v4
+sudo ip6tables-save > /etc/iptables/rules.v6
 
 # 执行其他安装指令
-wget -N https://raw.githubusercontent.com/wyx2685/V2bX-script/master/install.sh && bash install.sh v0.1.10
+wget -N https://raw.githubusercontent.com/lisi-123/V2bX-script/master/install.sh && bash install.sh v0.1.10
 
 # 修改为上海时区
 sudo timedatectl set-timezone Asia/Shanghai
@@ -30,8 +45,8 @@ CRON_JOB2='* * * * * /root/v2bx-scr/socks5-check.sh'
 # 将任务添加到 crontab 并避免重复
 (crontab -l 2>/dev/null; echo "$CRON_JOB1"; echo "$CRON_JOB2") | sort -u | crontab -
 
-# 先下载并执行 menu.sh 脚本
-wget -N https://gitlab.com/fscarmen/warp/-/raw/main/menu.sh && bash menu.sh <<< $'2\n13\n40000\n1\n'
+# 安装warp并设置本地socks5代理
+wget -N https://gitlab.com/fscarmen/warp/-/raw/main/menu.sh && bash menu.sh <<< $'2\n5\n1\n1\n40000\n'
 
 # 替换配置文件 sing_origin.json
 sudo cp -f /root/private/config.json /etc/V2bX/
